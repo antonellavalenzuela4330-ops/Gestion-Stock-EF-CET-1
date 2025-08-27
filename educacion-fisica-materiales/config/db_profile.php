f649fd43c68ae6fc5fbc22bbc2fb33ee76963cd1
antonellavalenzuela4330-ops <antonellavalenzuela4330@gmail.com>
2025-08-27 15:11:58 +0000

    new php

---
+++ educacion-fisica-materiales/config/db_profile.php
@@ -0,0 +1,18 @@
+<?php
+// Perfil de conexión para base de datos SQLite
+return [
+    'driver' => 'sqlite',
+    'database' => __DIR__ . '/../sql/materiales.sqlite',
+];
+
+// Ejemplo para MySQL (descomentar y editar si usas MySQL)
+/*
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'educacion_fisica',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
+*/
